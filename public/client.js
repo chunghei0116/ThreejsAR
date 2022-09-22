436689f8d0a34830c53332ca1ffe256638ca1cdc
Jones <chungheibrad@gmail.com>
2022-09-22 16:11:27 +0800

    hand input just for showing

--- public/client.js
+++ public/client.js
@@ -104,10 +104,7 @@ function initScene() {
   scene.add(controllerGrip1);
 
   hand1 = renderer.xr.getHand(0);
-  hand1.addEventListener("pinchstart", onPinchStartLeft);
-  hand1.addEventListener("pinchend", () => {
-    scaling.active = false;
-  });
+
   hand1.add(handModelFactory.createHandModel(hand1));
 
   scene.add(hand1);
@@ -120,8 +117,7 @@ function initScene() {
   );
   scene.add(controllerGrip2);
   hand2 = renderer.xr.getHand(1);
-  hand2.addEventListener("pinchstart", onPinchStartRight);
-  hand2.addEventListener("pinchend", onPinchEndRight);
+
   hand2.add(handModelFactory.createHandModel(hand2));
   scene.add(hand2);
   //
@@ -204,15 +200,6 @@ function render() {
   cleanIntersected();
   intersectObjects(controller1);
   intersectObjects(controller2);
-
-  if (scaling.active) {
-    const indexTip1Pos = hand1.joints["index-finger-tip"].position;
-    const indexTip2Pos = hand2.joints["index-finger-tip"].position;
-    const distance = indexTip1Pos.distanceTo(indexTip2Pos);
-    const newScale =
-      scaling.initialScale + distance / scaling.initialDistance - 1;
-    scaling.object.scale.setScalar(newScale);
-  }
 
   renderer.render(scene, camera);
 }
